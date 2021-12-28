e7d455fce3c95e187187d186802a2a9acf49a5b1
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-28 15:50:05 +0300

    Added test sample

--- test/built_rx_bloc_test.dart
+++ test/built_rx_bloc_test.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
-// import 'package:built_rx_bloc/built_rx_bloc.dart';
+import 'blocs/sum_bloc.dart';
 
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//   });
-// }
+void main() {
+  test('Sum bloc', () {
+    final sumBloc = SumBloc();
+    sumBloc.sum(20, 5);
+
+    expect(sumBloc.state.sum, equals(20 + 5));
+  });
+}
