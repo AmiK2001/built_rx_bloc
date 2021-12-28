0acc93cead1891716be8de00009b5d03e088a936
Amir Niyazov <Amir.Niyazov.2001.AN@gmail.com>
2021-12-28 11:48:14 +0300

    initial commit

---
+++ lib/built_rx_bloc.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:built_rx_bloc/utils/extensions/string_extensions.dart';
+import 'package:built_rx_bloc/utils/log.dart';
+import 'package:built_value/built_value.dart';
+import 'package:rxdart/rxdart.dart';
+
+abstract class Bloc<TState extends Built<TState, TStateBuilder>,
+    TStateBuilder extends Builder<TState, TStateBuilder>> {
+  Bloc(this._initialState, {this.debug = false}) {
+    log.d("constructor");
+
+    addState(_initialState);
+
+    init();
+  }
+
+  bool debug;
+  final stateTag = (TState).toString().toSnakecase();
+
+  final TState _initialState;
+  final _stateSubject = BehaviorSubject<TState>();
+  final _subscriptions = CompositeSubscription();
+
+  Stream<TState> get stateStream => _stateSubject.stream;
+
+  TState get state => _stateSubject.value;
+
+  TState get initialState => _initialState;
+
+  Future<void> init() {
+    return Future.value();
+  }
+
+  void dispose() {
+    _stateSubject.close();
+    _subscriptions.dispose();
+  }
+
+  void addState(TState state) {
+    if (!_stateSubject.isClosed) {
+      _stateSubject.add(state);
+    }
+
+    if (debug) log.d("$stateTag.addState\nstate: $state");
+  }
+
+  void updateState(Function(TStateBuilder b) updates) {
+    if (debug) {
+      log.d("$stateTag.updateState\npreviousState: $state");
+    }
+
+    addState(state.rebuild(updates));
+  }
+
+  StreamSubscription addSubscription(StreamSubscription streamSubscription) {
+    return _subscriptions.add(streamSubscription);
+  }
+}
---
+++ test/built_rx_bloc_test.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:built_rx_bloc/built_rx_bloc.dart';
+
+// void main() {
+//   test('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//   });
+// }
